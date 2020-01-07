a954dd98e184cf33c6ff7c334cb62f53ce81a5fc
kunal <Kunal Kumar Singh>
2020-01-07 23:44:45 +0530

    Leetcode commit

---
+++ SlidingWindowMaximum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print_max(int a[],int n,int k)
+{
+	int max_upto[n];
+
+}
+int main()
+{
+	int a[]={9,7,2,4,6,8,2,1,5};
+	int n=sizeof(a)/sizeof(int);
+	int k=3;
+	print_max(a,n,k);
+	return 0;
+}
